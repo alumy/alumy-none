200c42b64d3fc830770c30676678fb51866f68b7
jack <chenyj@jiuyunso.com>
2021-09-14 18:51:19 +0800

    docs(a): a

--- tools/commitizen_init.sh
+++ tools/commitizen_init.sh
@@ -2,7 +2,8 @@
 
 # Please make sure that you already installed the npm in your system
 
-sudo npm install -g commitizen conventional-changelog-cli
+# Please execute the next command to install commitizen for global
+# sudo npm install -g commitizen conventional-changelog-cli
 
 commitizen init cz-conventional-changelog --save --save-exact
 
