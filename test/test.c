2147a70abb75a441d8b9f54015179085ef1c9d33
jack <chenyj@jiuyunso.com>
2021-09-14 10:26:05 +0800

    format

--- test/test.c
+++ test/test.c
@@ -13,66 +13,66 @@ __BEGIN_DECLS
 
 static int example_suite_init(void)
 {
-	return 0;
+    return 0;
 }
 
 static int example_suite_clean(void)
 {
-	return 0;
+    return 0;
 }
 
 static void example_test(void)
 {
-	CU_ASSERT(1);
+    CU_ASSERT(1);
 }
 
 static void add_example_tests(void)
 {
-	CU_pSuite suite;
+    CU_pSuite suite;
 
-	suite = CU_add_suite("example", example_suite_init, example_suite_clean);
-	CU_add_test(suite, "example", example_test);
+    suite = CU_add_suite("example", example_suite_init, example_suite_clean);
+    CU_add_test(suite, "example", example_test);
 }
 
 static void add_tests(void)
 {
-	void (*add_test_tab[])(void) = {
-		add_example_tests,
-		add_mem_tests,
-		add_string_tests,
+    void (*add_test_tab[])(void) = {
+        add_example_tests,
+        add_mem_tests,
+        add_string_tests,
         add_filter_tests,
         add_bitband_tests,
-	};
+    };
 
-	assert(CU_get_registry() != NULL);
-	assert(!CU_is_test_running());
+    assert(CU_get_registry()!= NULL);
+    assert(!CU_is_test_running());
 
-	for (int32_t i = 0; i < ARRAY_SIZE(add_test_tab); ++i) {
-		if (add_test_tab[i] != NULL) {
-			add_test_tab[i]();
-		}
-	}
+    for (int32_t i = 0; i < ARRAY_SIZE(add_test_tab); ++i) {
+        if (add_test_tab[i] != NULL) {
+            add_test_tab[i]();
+        }
+    }
 }
 
 int main(int argc, char *argv[])
 {
-	if (CU_initialize_registry()) {
-		fprintf(stderr, "\nInitialization of Test Registry failed.");
-		return -1;
-	}
+    if (CU_initialize_registry()) {
+        fprintf(stderr, "\nInitialization of Test Registry failed.");
+        return -1;
+    }
 
-	add_tests();
+    add_tests();
 
     CU_set_output_filename("test");
     CU_list_tests_to_file();
     CU_automated_run_tests();
 
-	CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
 
-	CU_cleanup_registry();
+    CU_cleanup_registry();
 
-	return 0;
+    return 0;
 }
 
 __END_DECLS
