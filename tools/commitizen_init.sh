471fd637fc9a7c3c4b53a0d31092925ceb7f9f2e
jack <chenyj@jiuyunso.com>
2021-09-14 18:42:01 +0800

    docs(commitizen): update ghooks

--- tools/commitizen_init.sh
+++ tools/commitizen_init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo npm install -g commitizen ghooks validate-commit-msg conventional-changelog-cli
+sudo npm install -g commitizen conventional-changelog-cli
+
+npm install --save-dev validate-commit-msg
+npm install --save-dev ghooks 
 
 commitizen init cz-conventional-changelog --save --save-exact
 
