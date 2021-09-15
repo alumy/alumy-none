f201c95fffaaa2207dafda525d6b9b850c5d9660
jack <chenyj@jiuyunso.com>
2021-09-15 10:00:26 +0800

    build(commitizen): git push

--- tools/commitizen_init.sh
+++ tools/commitizen_init.sh
@@ -3,7 +3,7 @@
 # Please make sure that you already installed the npm in your system
 
 # Please execute the next command to install commitizen for global
-# sudo npm install -g commitizen conventional-changelog-cli
+npm install commitizen conventional-changelog-cli
 
 commitizen init cz-conventional-changelog --force --save --save-exact
 
