d02acfe5cf9ec5b8b506ca1b3cd980d7066d9cc6
jack <chenyj@jiuyunso.com>
2021-09-15 13:44:37 +0800

    build(commitizen): aaa

--- tools/commitizen_init.sh
+++ tools/commitizen_init.sh
@@ -3,10 +3,9 @@
 # Please make sure that you already installed the npm in your system
 
 # Please execute the next command to install commitizen for global by sudo user
-# sudo npm install -g commitizen conventional-changelog-cli
+# sudo npm install -g commitizen cz-conventional-changelog conventional-changelog conventional-changelog-cli validate-commit-msg
 
 commitizen init cz-conventional-changelog --force
 
-npm install validate-commit-msg
 npm install ghooks 
 
