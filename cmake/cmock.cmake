8224dc6b9f32ff4cb02f1e9831ded688abaaf290
jack <chenyj@jiuyunso.com>
2021-10-19 17:48:22 +0800

    test(cmock): add cmock

---
+++ cmake/cmock.cmake
@@ -0,0 +1,48 @@
+function(mock)
+    set(options)
+    set(single_value)
+    set(multi_value MOCK_HEADER_FILES INCLUDE_DIRS REQUIRES)
+	
+    cmake_parse_arguments(_ "${options}" "${single_value}" "${multi_value}" ${ARGN})
+
+    set(MOCK_GENERATED_HEADERS "")
+    set(MOCK_GENERATED_SRCS "")
+    set(MOCK_FILES "")
+    set(CMOCK_DIR "${PROJECT_SOURCE_DIR}/cmock/CMock")
+    set(MOCK_GEN_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+    list(APPEND __INCLUDE_DIRS "${MOCK_GEN_DIR}/mocks")
+
+    foreach(header_file ${__MOCK_HEADER_FILES})
+        get_filename_component(file_without_dir ${header_file} NAME_WE)
+        list(APPEND MOCK_GENERATED_HEADERS "${MOCK_GEN_DIR}/mocks/Mock${file_without_dir}.h")
+        list(APPEND MOCK_GENERATED_SRCS "${MOCK_GEN_DIR}/mocks/Mock${file_without_dir}.c")
+    endforeach()
+
+    file(MAKE_DIRECTORY "${MOCK_GEN_DIR}/mocks")
+
+    add_custom_command(
+        OUTPUT ruby_found SYMBOLIC
+        COMMAND "ruby" "-v"
+        COMMENT "Try to find ruby. If this fails, you need to install ruby"
+    )
+	
+    # This command builds the mocks.
+    # First, environment variable UNITY_DIR is set. This is necessary to prevent unity from looking in its own submodule
+    # which doesn't work in our CI yet...
+    # The rest is a straight forward call to cmock.rb, consult cmock's documentation for more information.
+    add_custom_target(mock
+        DEPENDS ruby_found
+        COMMAND ${CMAKE_COMMAND} -E env "UNITY_DIR=${PROJECT_SOURCE_DIR}/unity/unity"
+			ruby 
+			${CMOCK_DIR}/lib/cmock.rb 
+            -o${PROJECT_SOURCE_DIR}/mock/mock_config.yaml
+            ${__MOCK_HEADER_FILES}
+      )
+endfunction()
+
+#idf_component_mock(INCLUDE_DIRS ${include_dirs}
+#    REQUIRES esp_common
+#    MOCK_HEADER_FILES
+#    ${original_spi_flash_dir}/include/esp_partition.h
+#    ${original_spi_flash_dir}/include/esp_flash.h
+#    ${original_spi_flash_dir}/include/esp_spi_flash.h)
