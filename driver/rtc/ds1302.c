f49a474403827b3e84d0096125efd75fa70fb4b9
alumy <17096226@qq.com>
2021-11-18 23:06:22 +0800

    fixed the bug of ds1302 read and write year failed

--- driver/rtc/ds1302.c
+++ driver/rtc/ds1302.c
@@ -156,7 +156,7 @@ int_fast8_t ds1302_write_date_time(ds1302_t *this, const struct tm *tm)
     ds1302_write(this, 0x80, bin2bcd(tm->tm_sec));
     ds1302_write(this, 0x82, bin2bcd(tm->tm_min));
     /* bit7 set to 0 for 24 hours */
-    ds1302_write(this, 0x84, bin2bcd(tm->tm_hour) & 0x1F);
+    ds1302_write(this, 0x84, bin2bcd(tm->tm_hour) & 0x3F);
     ds1302_write(this, 0x86, bin2bcd(tm->tm_mday));
     ds1302_write(this, 0x88, bin2bcd(tm->tm_mon + 1));
     /* week day */
@@ -175,11 +175,25 @@ int_fast8_t ds1302_read_date_time(ds1302_t *this, struct tm *tm)
 
     tm->tm_sec = bcd2bin(ds1302_read(this, 0x81) & 0x7F);
     tm->tm_min = bcd2bin(ds1302_read(this, 0x83));
-    tm->tm_hour = bcd2bin(ds1302_read(this, 0x85) & 0x1F);
+    tm->tm_hour = bcd2bin(ds1302_read(this, 0x85) & 0x3F);
     tm->tm_mday = bcd2bin(ds1302_read(this, 0x87));
     tm->tm_mon = bcd2bin(ds1302_read(this, 0x89)) - 1;
     tm->tm_wday = bcd2bin(ds1302_read(this, 0x8B));
     tm->tm_year = bcd2bin(ds1302_read(this, 0x8D)) + 2000 - 1900;
+
+    if (((tm->tm_sec < 0) || (tm->tm_sec > 60)) ||
+        ((tm->tm_min < 0) || (tm->tm_min > 59)) ||
+        ((tm->tm_hour < 0) || (tm->tm_hour > 23)) ||
+        ((tm->tm_mday < 1) || (tm->tm_mday > 31)) ||
+        ((tm->tm_mon < 0) || (tm->tm_mon > 11))) {
+
+        tm->tm_year = 100;
+        tm->tm_mon = 0;
+        tm->tm_mday = 1;
+        tm->tm_hour = 0;
+        tm->tm_min = 0;
+        tm->tm_sec = 0;
+    }
 
     AL_DEBUG(AL_DRV_RTC_LOG, "%s, %04d-%02d-%02d %02d:%02d:%02d",
              __func__,
@@ -200,7 +214,7 @@ int_fast8_t ds1302_read_ram(ds1302_t *this, uint16_t addr,
 {
     uint8_t *p = (uint8_t *)buf;
     uint8_t __addr = (0xC1 + (addr << 1));
-    uint8_t __end = (0xC1 + ((size - 1) << 1));
+    uint8_t __end = (__addr + ((size - 1) << 1));
 
     BUG_ON(this == NULL);
     BUG_ON(buf == NULL);
@@ -220,7 +234,7 @@ int_fast8_t ds1302_write_ram(ds1302_t *this, uint16_t addr,
 {
     const uint8_t *p = (const uint8_t *)data;
     uint8_t __addr = (0xC0 + (addr << 1));
-    uint8_t __end = (0xC0 + ((len - 1) << 1));
+    uint8_t __end = (__addr + ((len - 1) << 1));
 
     BUG_ON(this == NULL);
     BUG_ON(data == NULL);
