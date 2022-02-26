c9dd992082580e5a7b238454a786c048229c55f0
jack <chenyj@jiuyunso.com>
2022-02-26 23:11:07 +0800

    feat(bcd): remove BUG_ON check

--- bcd/bcd.c
+++ bcd/bcd.c
@@ -8,16 +8,11 @@ __BEGIN_DECLS
 
 __attribute_const__ uint8_t bcd2bin(uint8_t bcd)
 {
-    BUG_ON(((bcd & 0xF0) >> 4) > 0x09);
-    BUG_ON((bcd & 0x0F) > 0x09);
-
     return (bcd & 0x0f) + (bcd >> 4) * 10;
 }
 
 __attribute_const__ uint8_t bin2bcd(uint8_t bin)
 {
-    BUG_ON(bin > 99);
-
     return ((bin / 10) << 4) | (bin % 10);
 }
 
