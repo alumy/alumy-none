26e203786e5a9b8479fcc040dee472ebf65beabc
jack <chenyj@jiuyunso.com>
2022-03-18 21:16:31 +0800

    feat(check): add check defines

---
+++ include/alumy/check.h
@@ -0,0 +1,32 @@
+#ifndef __AL_CHECK_H
+#define __AL_CHECK_H 1
+
+#include "alumy/config.h"
+#include "alumy/types.h"
+#include "alumy/base.h"
+#include "alumy/log.h"
+
+__BEGIN_DECLS
+
+#ifndef AL_CHECK_RET
+#define AL_CHECK_RET(exp, __errno, ret)     do {        \
+    BUG_ON(!(exp));                                     \
+    if(!(exp)) {                                        \
+        set_errno((__errno));                           \
+        return (ret);                                   \
+    }                                                   \
+}
+
+#ifndef AL_CHECK
+#define AL_CHECK(exp, __errno)     do {                 \
+    BUG_ON(!(exp));                                     \
+    if(!(exp)) {                                        \
+        set_errno((__errno));                           \
+        return;                                         \
+    }                                                   \
+}
+
+__END_DECLS
+
+#endif
+
