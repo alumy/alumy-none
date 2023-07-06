e77a1702bbdc00e8e702276c0de34881e0a51d14
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2023-07-06 14:49:52 +0800

    feat(version): v1.0.5.13

--- include/alumy/version.h
+++ include/alumy/version.h
@@ -11,8 +11,8 @@ __BEGIN_DECLS
 
 #define AL_MAJOR		1
 #define AL_MINOR		0
-#define AL_REV			4
-#define AL_BUILD		12
+#define AL_REV			5
+#define AL_BUILD		13
 
 #define AL_PREREQ(ma, mi)	(AL_MAJOR << 16 | AL_MINOR >= (ma) << 16 | (mi))
 #define AL_VERSION			__MS(AL_MAJOR.AL_MINOR.AL_REV.AL_BUILD)
