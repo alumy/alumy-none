35055b07acd588ee0781a12181c6b535075c2feb
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2023-07-06 13:21:39 +0800

    fix(git_hash): remove git_hash.h

--- include/alumy/git_hash.h
+++
@@ -1,7 +0,0 @@
-#ifndef __AL_GIT_HASH_H
-#define __AL_GIT_HASH_H 1
-
-#define AL_GIT_HASH	"7c742aa"
-
-#endif
-
--- include/alumy/svn_revision.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SVN_REVISION_H
-#define __SVN_REVISION_H 1
-
-#define SVN_REVISION		0
-
-#endif
-
