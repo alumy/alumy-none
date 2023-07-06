7c742aa329c49b78913be8175d31185adc3c0ce8
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2023-07-06 11:15:55 +0800

    fix(string.h): fixed the bug of miss time_t

---
+++ include/alumy/git_hash.h
@@ -0,0 +1,7 @@
+#ifndef __AL_GIT_HASH_H
+#define __AL_GIT_HASH_H 1
+
+#define AL_GIT_HASH	"2a2e150"
+
+#endif
+
---
+++ include/alumy/svn_revision.h
@@ -0,0 +1,7 @@
+#ifndef __SVN_REVISION_H
+#define __SVN_REVISION_H 1
+
+#define SVN_REVISION		0
+
+#endif
+
