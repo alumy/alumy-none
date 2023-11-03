0109ca3e63750a0049109fa90e2cf6f74109975f
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2023-11-03 16:16:03 +0800

    feat(filter): add kalman2

--- filter/kalman.c
+++ filter/kalman.c
@@ -21,15 +21,15 @@ __BEGIN_DECLS
  * @outputs
  * @retval
  */
-void al_kalman1_init(al_kalman1_t *kf,
+void al_kalman1_init(al_kalman1_t *k,
                      float x, float p, float a, float h, float q, float r)
 {
-    kf->x = x;
-    kf->p = p;
-    kf->a = a;
-    kf->h = h;
-    kf->q = q;       /* predict noise convariance */
-    kf->r = r;       /* measure error convariance */
+    k->x = x;
+    k->p = p;
+    k->a = a;
+    k->h = h;
+    k->q = q;       /* predict noise convariance */
+    k->r = r;       /* measure error convariance */
 }
 
 /**
@@ -42,19 +42,108 @@ void al_kalman1_init(al_kalman1_t *kf,
  * @retval
  *   Estimated result
  */
-float al_kalman1_filter(al_kalman1_t *kf, float measure)
+float al_kalman1_filter(al_kalman1_t *k, float measure)
 {
     /* Predict */
-    kf->x = kf->a * kf->x;
+    k->x = k->a * k->x;
     /* p(n|n-1)=A^2*p(n-1|n-1)+q */
-    kf->p = kf->a * kf->a * kf->p + kf->q;
+    k->p = k->a * k->a * k->p + k->q;
 
     /* Measurement */
-    kf->gain = kf->p * kf->h / (kf->p * kf->h * kf->h + kf->r);
-    kf->x = kf->x + kf->gain * (measure - kf->h * kf->x);
-    kf->p = (1 - kf->gain * kf->h) * kf->p;
+    k->gain = k->p * k->h / (k->p * k->h * k->h + k->r);
+    k->x = k->x + k->gain * (measure - k->h * k->x);
+    k->p = (1 - k->gain * k->h) * k->p;
 
-    return kf->x;
+    return k->x;
+}
+
+/*
+ * @brief
+ *   Init fields of structure @kalman1_state.
+ *   I make some defaults in this init function:
+ *     A = {{1, 0.1}, {0, 1}};
+ *     H = {1,0};
+ *   and @q,@r are valued after prior tests.
+ *
+ *   NOTES: Please change A,H,q,r according to your application.
+ *
+ * @inputs
+ * @outputs
+ * @retval
+ */
+void al_kalman2_init(al_kalman2_t *k,
+					 const float x[2], const float p[2][2],
+					 const float a[2][2], const float h[2],
+					 const float q[2], float r)
+{
+    k->x[0] = x[0];
+    k->x[1] = x[1];
+
+	k->p[0][0] = p[0][0];
+    k->p[0][1] = p[0][1];
+    k->p[1][0] = p[1][0];
+    k->p[1][1] = p[1][1];
+
+    k->a[0][0] = a[0][0];
+    k->a[0][1] = a[0][1];
+    k->a[1][0] = a[1][0];
+    k->a[1][1] = a[1][1];
+
+    k->h[0] = h[0];
+    k->h[1] = h[1];
+
+    k->q[0] = q[0];
+    k->q[1] = q[1];
+    k->r = r;  /* estimated error convariance */
+}
+
+/*
+ * @brief
+ *   2 Dimension kalman filter
+ * @inputs
+ *   state - Klaman filter structure
+ *   z_measure - Measure value
+ * @outputs
+ *   state->x[0] - Updated state value, Such as angle,velocity
+ *   state->x[1] - Updated state value, Such as diffrence angle, acceleration
+ *   state->p    - Updated estimated error convatiance matrix
+ * @retval
+ *   Return value is equals to state->x[0], so maybe angle or velocity.
+ */
+float al_kalman2_filter(al_kalman2_t *k, float measure)
+{
+	float temp0 = 0.0f;
+	float temp1 = 0.0f;
+	float temp = 0.0f;
+
+	/* Step1: Predict */
+	k->x[0] = k->a[0][0] * k->x[0] + k->a[0][1] * k->x[1];
+	k->x[1] = k->a[1][0] * k->x[0] + k->a[1][1] * k->x[1];
+	/* p(n|n-1)=A^2*p(n-1|n-1)+q */
+	k->p[0][0] = k->a[0][0] * k->p[0][0] + k->a[0][1] * k->p[1][0] + k->q[0];
+	k->p[0][1] = k->a[0][0] * k->p[0][1] + k->a[1][1] * k->p[1][1];
+	k->p[1][0] = k->a[1][0] * k->p[0][0] + k->a[0][1] * k->p[1][0];
+	k->p[1][1] = k->a[1][0] * k->p[0][1] + k->a[1][1] * k->p[1][1] + k->q[1];
+
+	/* Step2: Measurement */
+	/* gain = p * H^T * [r + H * p * H^T]^(-1), H^T means transpose. */
+	temp0 = k->p[0][0] * k->h[0] + k->p[0][1] * k->h[1];
+	temp1 = k->p[1][0] * k->h[0] + k->p[1][1] * k->h[1];
+	temp = k->r + k->h[0] * temp0 + k->h[1] * temp1;
+	k->gain[0] = temp0 / temp;
+	k->gain[1] = temp1 / temp;
+	/* x(n|n) = x(n|n-1) + gain(n) * [measure - H(n)*x(n|n-1)]*/
+	temp = k->h[0] * k->x[0] + k->h[1] * k->x[1];
+	k->x[0] = k->x[0] + k->gain[0] *(measure - temp);
+	k->x[1] = k->x[1] + k->gain[1] *(measure - temp);
+
+	/* Update @p: p(n|n) = [I - gain * H] * p(n|n-1) */
+	k->p[0][0] = (1 - k->gain[0] * k->h[0])* k->p[0][0];
+	k->p[0][1] = (1 - k->gain[0] * k->h[1])* k->p[0][1];
+	k->p[1][0] = (1 - k->gain[1] * k->h[0])* k->p[1][0];
+	k->p[1][1] = (1 - k->gain[1] * k->h[1])* k->p[1][1];
+
+	return k->x[0];
 }
 
 __END_DECLS
--- include/alumy/filter/kalman.h
+++ include/alumy/filter/kalman.h
@@ -9,6 +9,10 @@ __BEGIN_DECLS
 
 #define KALMAN_Q        0.01
 #define KALMAN_R        10.00
+#define AL_KALMAN2_A		{ {1, 0.1}, {0, 1} }
+#define AL_KALMAN2_H		{ 1, 0 }
+#define AL_KALMAN2_Q		{ { 10e-6, 0 }, { 0, 10e-6 } }
+#define AL_KALMAN2_R		(10e-7)
 
 /*
  * NOTES: n Dimension means the state is n dimension,
@@ -26,10 +30,28 @@ typedef struct al_kalman1 {
     float gain;
 } al_kalman1_t;
 
+/* 2 Dimension */
+typedef struct al_kalman2 {
+    float x[2];     /* state: [0]-angle [1]-diffrence of angle, 2x1 */
+    float a[2][2];  /* X(n)=A*X(n-1)+U(n),U(n)~N(0,q), 2x2 */
+    float h[2];     /* Z(n)=H*X(n)+W(n),W(n)~N(0,r), 1x2   */
+    float q[2];     /* process(predict) noise convariance,2x1 [q0,0; 0,q1] */
+    float r;        /* measure noise convariance */
+    float p[2][2];  /* estimated error convariance,2x2 [p0 p1; p2 p3] */
+    float gain[2];  /* 2x1 */
+} al_kalman2_t;
+
 void al_kalman1_init(al_kalman1_t *kf,
                      float x, float p, float a, float h, float q, float r);
 
 float al_kalman1_filter(al_kalman1_t *kf, float measure);
+
+void al_kalman2_init(al_kalman2_t *k,
+					 const float x[2], const float p[2][2],
+					 const float a[2][2], const float h[2],
+					 const float q[2], float r);
+
+float al_kalman2_filter(al_kalman2_t *k, float measure);
 
 __END_DECLS
 
