74cda51b580102d7f272dfc561e667b66bd927f6
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2024-03-27 14:15:02 +0800

    feat: ascii
    add asc2hex

--- ascii.c
+++ ascii.c
@@ -5,16 +5,5 @@
 
 __BEGIN_DECLS
 
-const char al_hex_asc_tab[16] = "0123456789ABCDEF";
-
-uint8_t al_hex2asc(uint8_t value)
-{
-	if (value < ARRAY_SIZE(al_hex_asc_tab)) {
-		return al_hex_asc_tab[value];
-	}
-
-	return 0;
-}
-
 __END_DECLS
 
--- include/alumy/ascii.h
+++ include/alumy/ascii.h
@@ -38,7 +38,36 @@ __BEGIN_DECLS
 #define AL_EOF		(-1)
 #endif
 
-extern uint8_t al_hex2asc(uint8_t value);
+__static_inline__ char al_hex2asc(uint8_t value)
+{
+    static const char al_hex_asc_tab[16] = "0123456789ABCDEF";
+
+    if (__likely(value < ARRAY_SIZE(al_hex_asc_tab))) {
+        return al_hex_asc_tab[value];
+    }
+
+    return 0;
+}
+
+__static_inline__ uint8_t al_asc2hex(char asc)
+{
+    static const uint8_t al_asc_hex_tab[128] = {
+        ['0'] = 0x00, ['1'] = 0x01, ['2'] = 0x02, ['3'] = 0x03,
+        ['4'] = 0x04, ['5'] = 0x05, ['6'] = 0x06, ['7'] = 0x07,
+        ['8'] = 0x08, ['9'] = 0x09, ['A'] = 0x0A, ['B'] = 0x0B,
+        ['C'] = 0x0C, ['D'] = 0x0D, ['E'] = 0x0E, ['F'] = 0x0F,
+        ['a'] = 0x0A, ['b'] = 0x0B, ['c'] = 0x0C, ['d'] = 0x0D,
+        ['e'] = 0x0E, ['f'] = 0x0F,
+    };
+
+    uint_t off = asc;
+
+    if (__likely(off < ARRAY_SIZE(al_asc_hex_tab))) {
+        return al_asc_hex_tab[off];
+    }
+
+    return 0;
+}
 
 __END_DECLS
 
