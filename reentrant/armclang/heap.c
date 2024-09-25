c37436a7b07710df2d36f2dd6cfe3db914a40509
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2024-09-25 15:18:18 +0800

    feat(reentrant): add reeentrant

---
+++ reentrant/armclang/heap.c
@@ -0,0 +1,27 @@
+#include "durian.h"
+
+__BEGIN_DECLS
+
+__asm(".global __use_no_heap_region\n\t");
+
+void *malloc (size_t size)
+{
+	return al_os_malloc(size);
+}
+
+void free(void* p)
+{
+	al_os_free(p);
+}
+
+void *realloc(void* p,size_t want)
+{
+	return al_os_realloc(p, want);
+}
+
+void *calloc(size_t nmemb, size_t size)
+{
+	return al_os_calloc(nmemb, size);
+}
+
+__END_DECLS
