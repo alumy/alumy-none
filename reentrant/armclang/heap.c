7c9496efe2ff831475b4223fd2ac6358540757a3
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2024-09-25 15:34:20 +0800

    style(osal): format

--- reentrant/armclang/heap.c
+++ reentrant/armclang/heap.c
@@ -4,24 +4,24 @@ __BEGIN_DECLS
 
 __asm(".global __use_no_heap_region\n\t");
 
-void *malloc (size_t size)
+void *malloc(size_t size)
 {
-	return al_os_malloc(size);
+    return al_os_malloc(size);
 }
 
-void free(void* p)
+void free(void *p)
 {
-	al_os_free(p);
+    al_os_free(p);
 }
 
-void *realloc(void* p,size_t want)
+void *realloc(void *p, size_t want)
 {
-	return al_os_realloc(p, want);
+    return al_os_realloc(p, want);
 }
 
 void *calloc(size_t nmemb, size_t size)
 {
-	return al_os_calloc(nmemb, size);
+    return al_os_calloc(nmemb, size);
 }
 
 __END_DECLS
