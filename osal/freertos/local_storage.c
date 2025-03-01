d3530e22491f086086b352b44ef918c3e70de6fd
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2025-03-01 12:05:42 +0800

    feat(osal): every task use it self log buffer

--- osal/freertos/local_storage.c
+++ osal/freertos/local_storage.c
@@ -2,19 +2,16 @@
 #include "task.h"
 #include "timers.h"
 #include "semphr.h"
-#include "alumy/config.h"
-#include "alumy/byteorder.h"
-#include "alumy/types.h"
-#include "alumy/base.h"
-#include "alumy/bug.h"
-#include "alumy/osal.h"
-#include "alumy/errno.h"
+#include "alumy.h"
+#include "SEGGER_RTT.h"
 
 __BEGIN_DECLS
 
 #define LSP_ERRNO_INDEX     0
+#define LSP_LOGBUF_INDEX    1
 
-#if (configNUM_THREAD_LOCAL_STORAGE_POINTERS >= 1)
+#if (configNUM_THREAD_LOCAL_STORAGE_POINTERS >= 2)
+
 int *__al_errno(void)
 {
     /* in the interrupt or scheduler is not running */
@@ -36,6 +33,44 @@ int *__al_errno(void)
 
     return (int *)e;
 }
+
+void al_vlog(int32_t pri, const char *fmt, va_list ap)
+{
+#define VLOG_SIZE       128
+    static char buf[VLOG_SIZE];
+    ssize_t n;
+	
+    snprintf(buf, sizeof(buf), "[%5u.%03u]", al_uptime(), al_tick_get_msec());
+
+    /* Print timestamp first */
+    SEGGER_RTT_printf(0, "%s ", buf);
+
+    /* Check if in interrupt or scheduler not running */
+    if (__unlikely(vPortGetIPSR() || (xTaskGetCurrentTaskHandle() == NULL))) {
+        n = vsnprintf(buf, sizeof(buf), fmt, ap);
+        if (n > 0)
+            SEGGER_RTT_Write(0, buf, min((ssize_t)VLOG_SIZE - 1, n));
+		
+        return;
+    }
+
+    /* get the current task storage */
+    char *logbuf = pvTaskGetThreadLocalStoragePointer(NULL, LSP_LOGBUF_INDEX);
+    if (logbuf == NULL) {
+        logbuf = al_os_malloc(VLOG_SIZE);
+        vTaskSetThreadLocalStoragePointer(NULL, LSP_LOGBUF_INDEX, logbuf);
+
+        logbuf = pvTaskGetThreadLocalStoragePointer(NULL, LSP_LOGBUF_INDEX);
+        BUG_ON(logbuf == NULL);
+    }
+
+    /* In task context */
+    n = vsnprintf(logbuf, VLOG_SIZE, fmt, ap);
+    if (n > 0)
+        SEGGER_RTT_Write(0, logbuf, min((ssize_t)VLOG_SIZE - 1, n));
+#undef VLOG_SIZE
+}
+
 #else
 #error("configNUM_THREAD_LOCAL_STORAGE_POINTERS must greather than 1 and \
 the index 0 is reserved for errno");
