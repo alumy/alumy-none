7894324345a0cf4dbc7cb89d741169476a08875a
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2025-05-13 15:16:03 +0800

    fix(osal): update interface

--- thread/worker.c
+++ thread/worker.c
@@ -18,7 +18,6 @@ int32_t al_worker_init(al_worker_t *ctx, int_t cnt,
 {
 	int32_t ret;
 
-	ctx->task = NULL;
 	ctx->queue = NULL;
 
 	if (!al_initialized_init(&ctx->init)) {
@@ -29,7 +28,7 @@ int32_t al_worker_init(al_worker_t *ctx, int_t cnt,
 	ctx->queue = al_os_queue_create(cnt, sizeof(al_worker_item_t));
 	AL_CHECK_RET(ctx->queue, ENOMEM, -1);
 
-	ret = al_os_task_create(ctx->task, name, prio, stack, al_worker_task, ctx);
+	ret = al_os_task_create(&ctx->task, name, prio, stack, al_worker_task, ctx);
 	AL_CHECK_RET(ret == 0, ENOMEM, -1);
 
 	return 0;
