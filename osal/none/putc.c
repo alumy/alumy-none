56080517dda08105ca5333691aa0d997562d610d
chenyj@jiuyunso.com <chenyj@jiuyunso.com>
2025-06-20 14:12:00 +0800

    feat(osal): update osal

--- osal/none/putc.c
+++ osal/none/putc.c
@@ -3,13 +3,11 @@
 #include "alumy/types.h"
 #include "alumy/base.h"
 #include "alumy/osal.h"
-#include "SEGGER_RTT.h"
 
 __BEGIN_DECLS
 
 __weak void al_putc(int_t c)
 {
-    SEGGER_RTT_PutChar(0, c);
 }
 
 __END_DECLS
